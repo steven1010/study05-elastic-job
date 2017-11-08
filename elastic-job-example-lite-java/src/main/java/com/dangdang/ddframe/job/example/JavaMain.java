a2295f4e21b8e878dc1713b5d05514173280ce9b
ropert911 <ropert911@163.com>
2017-11-08 11:22:04 +0800

    处理编译问题

--- elastic-job-example-lite-java/src/main/java/com/dangdang/ddframe/job/example/JavaMain.java
+++ elastic-job-example-lite-java/src/main/java/com/dangdang/ddframe/job/example/JavaMain.java
@@ -40,28 +40,28 @@ import java.nio.file.Paths;
 import java.nio.file.attribute.PosixFilePermissions;
 
 public final class JavaMain {
-    
+
     private static final int EMBED_ZOOKEEPER_PORT = 4181;
-    
+
     private static final String ZOOKEEPER_CONNECTION_STRING = "localhost:" + EMBED_ZOOKEEPER_PORT;
-    
+
     private static final String JOB_NAMESPACE = "elastic-job-example-lite-java";
-    
+
     // switch to MySQL by yourself
 //    private static final String EVENT_RDB_STORAGE_DRIVER = "com.mysql.jdbc.Driver";
 //    private static final String EVENT_RDB_STORAGE_URL = "jdbc:mysql://localhost:3306/elastic_job_log";
-    
+
     private static final String EVENT_RDB_STORAGE_DRIVER = "org.h2.Driver";
-    
+
     private static final String EVENT_RDB_STORAGE_URL = "jdbc:h2:mem:job_event_storage";
-    
+
     private static final String EVENT_RDB_STORAGE_USERNAME = "sa";
-    
+
     private static final String EVENT_RDB_STORAGE_PASSWORD = "";
-    
+
     // CHECKSTYLE:OFF
     public static void main(final String[] args) throws IOException {
-    // CHECKSTYLE:ON
+        // CHECKSTYLE:ON
         EmbedZookeeperServer.start(EMBED_ZOOKEEPER_PORT);
         CoordinatorRegistryCenter regCenter = setUpRegistryCenter();
         JobEventConfiguration jobEventConfig = new JobEventRdbConfiguration(setUpEventTraceDataSource());
@@ -69,14 +69,14 @@ public final class JavaMain {
         setUpDataflowJob(regCenter, jobEventConfig);
         setUpScriptJob(regCenter, jobEventConfig);
     }
-    
+
     private static CoordinatorRegistryCenter setUpRegistryCenter() {
         ZookeeperConfiguration zkConfig = new ZookeeperConfiguration(ZOOKEEPER_CONNECTION_STRING, JOB_NAMESPACE);
         CoordinatorRegistryCenter result = new ZookeeperRegistryCenter(zkConfig);
         result.init();
         return result;
     }
-    
+
     private static DataSource setUpEventTraceDataSource() {
         BasicDataSource result = new BasicDataSource();
         result.setDriverClassName(EVENT_RDB_STORAGE_DRIVER);
@@ -85,25 +85,29 @@ public final class JavaMain {
         result.setPassword(EVENT_RDB_STORAGE_PASSWORD);
         return result;
     }
-    
+
     private static void setUpSimpleJob(final CoordinatorRegistryCenter regCenter, final JobEventConfiguration jobEventConfig) {
-        JobCoreConfiguration coreConfig = JobCoreConfiguration.newBuilder("javaSimpleJob", "0/5 * * * * ?", 3).shardingItemParameters("0=Beijing,1=Shanghai,2=Guangzhou").build();
+        JobCoreConfiguration coreConfig = JobCoreConfiguration.newBuilder("javaSimpleJob", "0/5 * * * * ?", 3).
+                shardingItemParameters("0=Beijing,1=Shanghai,2=Guangzhou")
+                .build();
         SimpleJobConfiguration simpleJobConfig = new SimpleJobConfiguration(coreConfig, JavaSimpleJob.class.getCanonicalName());
         new JobScheduler(regCenter, LiteJobConfiguration.newBuilder(simpleJobConfig).build(), jobEventConfig).init();
     }
-    
+
     private static void setUpDataflowJob(final CoordinatorRegistryCenter regCenter, final JobEventConfiguration jobEventConfig) {
-        JobCoreConfiguration coreConfig = JobCoreConfiguration.newBuilder("javaDataflowElasticJob", "0/5 * * * * ?", 3).shardingItemParameters("0=Beijing,1=Shanghai,2=Guangzhou").build();
+        JobCoreConfiguration coreConfig = JobCoreConfiguration.newBuilder("javaDataflowElasticJob", "0/5 * * * * ?", 3)
+                .shardingItemParameters("0=Beijing,1=Shanghai,2=Guangzhou")
+                .build();
         DataflowJobConfiguration dataflowJobConfig = new DataflowJobConfiguration(coreConfig, JavaDataflowJob.class.getCanonicalName(), true);
         new JobScheduler(regCenter, LiteJobConfiguration.newBuilder(dataflowJobConfig).build(), jobEventConfig).init();
     }
-    
+
     private static void setUpScriptJob(final CoordinatorRegistryCenter regCenter, final JobEventConfiguration jobEventConfig) throws IOException {
         JobCoreConfiguration coreConfig = JobCoreConfiguration.newBuilder("scriptElasticJob", "0/5 * * * * ?", 3).build();
         ScriptJobConfiguration scriptJobConfig = new ScriptJobConfiguration(coreConfig, buildScriptCommandLine());
         new JobScheduler(regCenter, LiteJobConfiguration.newBuilder(scriptJobConfig).build(), jobEventConfig).init();
     }
-    
+
     private static String buildScriptCommandLine() throws IOException {
         if (System.getProperties().getProperty("os.name").contains("Windows")) {
             return Paths.get(JavaMain.class.getResource("/script/demo.bat").getPath().substring(1)).toString();
